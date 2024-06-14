e1b6d84dca4aeb238cf1bd73c2db3dd9f79abd94
kelly15012 <kellylimyaujade@gmail.com>
2024-06-14 11:32:41 +0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Bao.Data;
+using Bao.Areas.Identity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BaoContextConnection") ?? throw new InvalidOperationException("Connection string 'BaoContextConnection' not found.");
+
+builder.Services.AddDbContext<BaoContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<BaoUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BaoContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
