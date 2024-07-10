26484f3d7f1a50b8940452402e33687e03618adc
IMELDA <imeldachuahy@gmail.com>
2024-07-10 19:40:49 +0800

    update identity

---
+++ Models/Staff.cs
@@ -0,0 +1,12 @@
+﻿namespace Bao.Models
+{
+    public class Staff
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+
+
+    }
+}
