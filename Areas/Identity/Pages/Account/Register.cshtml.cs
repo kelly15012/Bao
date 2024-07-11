eeabe23ec19aaf947871d6a527502e19e46855dd
Lennon <lennontyc@gmail.com>
2024-07-11 14:37:15 +0800

    add role assignment code

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -160,8 +160,8 @@ namespace Bao.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    //var roleRsult = await _userManager.AddToRoleAsync(user, "role name");
                     _logger.LogInformation("User created a new account with password.");
-
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
