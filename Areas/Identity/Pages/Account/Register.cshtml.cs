ffb3ce84c30906dee8c34955f1cf7163b6f750c5
kelly15012 <kellylimyaujade@gmail.com>
2024-07-14 02:33:31 +0800

    register assigned to role userrole configured (Baozi only)

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -162,6 +162,7 @@ namespace Bao.Areas.Identity.Pages.Account
                 {
                     // var roleRsult = await _userManager.AddToRoleAsync(user, "role name");
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "Baozi");
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
