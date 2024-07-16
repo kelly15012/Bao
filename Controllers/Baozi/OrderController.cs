b4bbf1a79f1e864c089b3082f1e555538f70cf2b
kelly15012 <kellylimyaujade@gmail.com>
2024-07-16 20:12:21 +0800

    update order detilas and layout nav

---
+++ Controllers/Baozi/OrderController.cs
@@ -0,0 +1,68 @@
+﻿using Bao.Areas.Identity.Data;
+using Bao.Data;
+using Bao.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bao.Controllers.Baozi
+{
+    [Authorize(Roles = "Baozi")]
+    public class OrderController : Controller
+    {
+        private readonly BaoContext _context;
+        private readonly UserManager<BaoUser> _userManager;
+
+        public OrderController(BaoContext context, UserManager<BaoUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Order
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var userId = user.Id;
+            var orders = await _context.Orders
+                                       .Where(o => o.UserId == userId)
+                                       .Include(o => o.OrderItems!)
+                                       .ThenInclude(oi => oi.Product)
+                                       .ToListAsync();
+
+            return View("~/Views/Baozi/Order.cshtml", orders);
+        }
+
+        // GET: Order/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var userId = user.Id;
+            var order = await _context.Orders
+                                      .Where(o => o.OrderId == id && o.UserId == userId)
+                                      .Include(o => o.OrderItems!)
+                                      .ThenInclude(oi => oi.Product)
+                                      .FirstOrDefaultAsync();
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View("~/Views/Baozi/OrderDetails.cshtml", order);
+        }
+    }
+}
