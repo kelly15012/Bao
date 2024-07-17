7efac5b1fd9b8e70504f42f7dfd20d5f642d82cb
Kel <kellylimyaujade@gmail.com>
2024-07-18 02:44:03 +0800

    ADD:admin dashboard

--- Controllers/Admin/AdminController.cs
+++ Controllers/Admin/AdminController.cs
@@ -1,16 +1,14 @@
-﻿using Bao.Areas.Identity.Data;
-using Bao.Data;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Bao.Data;
+using Bao.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bao.Controllers.Admin
 {
-    [Authorize(Roles = "Admin")]
+    [Route("Admin/[controller]")]
+    [ApiController]
     public class AdminController : Controller
     {
         private readonly BaoContext _context;
@@ -20,39 +18,56 @@ namespace Bao.Controllers.Admin
             _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult aLanding()
-        {
-            return View();
-        }
-
-        public IActionResult adminDashboard()
-        {
-            return View();
-        }
-
-        // API to get order data for the past three days
-        [HttpGet]
-        [Route("api/orders/last-three-days")]
+        [HttpGet("last-three-days")]
         public async Task<IActionResult> GetOrdersForLastThreeDays()
         {
-            var fromDate = DateTime.Now.AddDays(-3);
-
+            var threeDaysAgo = DateTime.Now.AddDays(-3);
             var orders = await _context.Orders
-                .Where(o => o.OrderDate >= fromDate)
-                .GroupBy(o => o.OrderDate.Date)
-                .Select(g => new
-                {
-                    Date = g.Key,
-                    TotalAmount = g.Sum(o => o.TotalAmount)
-                })
-                .ToListAsync();
+                                       .Where(o => o.OrderDate >= threeDaysAgo)
+                                       .GroupBy(o => o.OrderDate.Date)
+                                       .Select(g => new
+                                       {
+                                           Date = g.Key,
+                                           TotalAmount = g.Sum(o => o.TotalAmount)
+                                       })
+                                       .ToListAsync();
 
-            return Json(orders);
+            return Ok(orders);
+        }
+
+        [HttpGet("category-sales")]
+        public async Task<IActionResult> GetCategorySales()
+        {
+            var categorySales = await _context.OrderItems
+                                              .Include(oi => oi.Product)
+                                              .ThenInclude(p => p.Category)
+                                              .GroupBy(oi => oi.Product!.Category!.categoryName)
+                                              .Select(g => new
+                                              {
+                                                  Category = g.Key,
+                                                  TotalSales = g.Sum(oi => oi.Quantity * oi.UnitPrice)
+                                              })
+                                              .ToListAsync();
+
+            return Ok(categorySales);
+        }
+
+        [HttpGet("dashboard")]
+        public IActionResult AdminDashboard()
+        {
+            return View("~/Views/Admin/adminDashboard.cshtml");
+        }
+
+        [HttpGet("landing")]
+        public IActionResult ALanding()
+        {
+            return View("~/Views/Admin/aLanding.cshtml");
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Index.cshtml");
         }
     }
 }
