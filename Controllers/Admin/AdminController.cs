126194e1861675bcef0f365fc64035c1035adb57
Kel <kellylimyaujade@gmail.com>
2024-07-18 02:06:45 +0800

    UPDDATE:Admin dashboard

--- Controllers/Admin/AdminController.cs
+++ Controllers/Admin/AdminController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Bao.Areas.Identity.Data;
+using Bao.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Bao.Controllers.Admin
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly BaoContext _context;
+
+        public AdminController(BaoContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +30,29 @@ namespace Bao.Controllers.Admin
             return View();
         }
 
-        
+        public IActionResult adminDashboard()
+        {
+            return View();
+        }
+
+        // API to get order data for the past three days
+        [HttpGet]
+        [Route("api/orders/last-three-days")]
+        public async Task<IActionResult> GetOrdersForLastThreeDays()
+        {
+            var fromDate = DateTime.Now.AddDays(-3);
+
+            var orders = await _context.Orders
+                .Where(o => o.OrderDate >= fromDate)
+                .GroupBy(o => o.OrderDate.Date)
+                .Select(g => new
+                {
+                    Date = g.Key,
+                    TotalAmount = g.Sum(o => o.TotalAmount)
+                })
+                .ToListAsync();
+
+            return Json(orders);
+        }
     }
 }
