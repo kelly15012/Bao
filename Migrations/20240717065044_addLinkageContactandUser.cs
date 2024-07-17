d2135c2d341c1f133249af9462e8e39f2576c6f9
Kel <kellylimyaujade@gmail.com>
2024-07-17 14:51:30 +0800

    update db

---
+++ Migrations/20240717065044_addLinkageContactandUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bao.Migrations
+{
+    /// <inheritdoc />
+    public partial class addLinkageContactandUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
